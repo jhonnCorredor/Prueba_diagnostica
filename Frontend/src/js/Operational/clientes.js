bc7d6f878558da87c721a0ff34b9a95c773496c7
jhonnCorredor <jhoncorredormedina29@gmail.com>
2024-05-10 17:48:08 -0500

    Modulo 1

--- Frontend/src/js/Operational/clientes.js
+++ Frontend/src/js/Operational/clientes.js
@@ -29,9 +29,6 @@ function loadData(){
     })
 }
 
-function registro() {
-}
-
 function save() {
     try {
         var data = {
@@ -45,12 +42,116 @@ function save() {
             "estado": $("#estado").val()
         }
 
+        var jsonData = JSON.stringify(data)
+
         $.ajax({
             url:"http://localhost:9000/prueba-diagnostica/v1/api/cliente",
             method: "POST",
             dataType: "json",
+            contentType: "aplicattion/json",
+            data: jsonData,
+            success: function (data) {
+                Swal.fire({
+                    title: "Registro exitoso",
+                    text: data.message,
+                    icon: "success"
+                  });
+            },
+            error: function (error) {
+                console.error(error);
+            }
         })
     } catch (error) {
         
     }
+}
+
+function findById(id) {
+    $.ajax({
+        url:"http://localhost:9000/prueba-diagnostica/v1/api/cliente/"+id,
+        method: "GET",
+        dataType: "json",
+        success: function (response) {
+            var data = response.data;
+            $("#nombre_cliente").val(data.nombre_cliente),
+            $("#apellido_cliente").val(data.apellido_Cliente),
+            $("#t_identificacion").val(data.tipo_identificacion),
+            $("#identificacion").val(data.identificacion),
+            $("#telefono").val(data.telefono),
+            $("#direccion").val(data.direccion),
+            $("#ciudad").val(data.ciudad),
+            $("#estado").val(data.estado)
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    })   
+}
+
+function update(id) {
+    try {
+        var data = {
+            "nombre_cliente": $("#nombre_cliente").val(),
+            "apellido_Cliente": $("#apellido_cliente").val(),
+            "tipo_identificacion": $("#t_identificacion").val(),
+            "identificacion": $("#identificacion").val(),
+            "telefono": $("#telefono").val(),
+            "direccion": $("#direccion").val(),
+            "ciudad": $("#ciudad").val(),
+            "estado": $("#estado").val()
+        }
+
+        var jsonData = JSON.stringify(data)
+
+        $.ajax({
+            url:"http://localhost:9000/prueba-diagnostica/v1/api/cliente/" + id,
+            method: "PUT",
+            dataType: "json",
+            contentType: "aplicattion/json",
+            data: jsonData,
+            success: function (data) {
+                Swal.fire({
+                    title: "Registro actualizado",
+                    text: data.message,
+                    icon: "success"
+                  });
+            },
+            error: function (error) {
+                console.error(error);
+            }
+        })
+    } catch (error) {
+        
+    }
+}
+
+function deleted(id) {
+    Swal.fire({
+        title: "Seguro eliminar registro?",
+        text: "No se puede revertir la siguiente acción!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "SI!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url:"http://localhost:9000/prueba-diagnostica/v1/api/cliente/" + id,
+                method: "DELETE",
+                dataType: "json",
+                contentType: "aplicattion/json",
+                success: function (data) {
+                    Swal.fire({
+                        title: "Registro Eliminado",
+                        text: data.message,
+                        icon: "success"
+                      });
+                },
+                error: function (error) {
+                    console.error(error);
+                }
+            }) 
+        }
+      });
 }
